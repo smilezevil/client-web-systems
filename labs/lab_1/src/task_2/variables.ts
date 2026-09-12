f1900888c1c7903665343cfd831da75f09da0b0a
smilezevil <demyanchuknastya2018@gmail.com>
2026-09-12 22:14:23 +0300

    task_2: variables.ts and functions.ts with typed variables, arrays and default parameters

---
+++ labs/lab_1/src/task_2/variables.ts
@@ -0,0 +1,18 @@
+// Змінні з явним типом
+let userName: string = "Anastasiia";
+let userAge: number = 18;
+let isStudent: boolean = true;
+let something: any = "kakashka";
+
+// Виводимо значення в консоль
+console.log("Ім'я:", userName);
+console.log("Вік:", userAge);
+console.log("Студент:", isStudent);
+console.log("Змінна any:", something);
+
+// Масиви з конкретними типами елементів
+let names: string[] = ["Анна", "Іван", "Марія"];
+let scores: number[] = [90, 85, 78];
+
+console.log("Масив імен:", names);
+console.log("Масив оцінок:", scores);
